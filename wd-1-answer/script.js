d761e5965fac99d21d03d4b60e9df63ad2b3baa6
Its-utsav <utsavdhimmar987@gmail.com>
2025-02-09 18:28:04 +0530

    Initial commit

---
+++ wd-1-answer/script.js
@@ -0,0 +1,15 @@
+function checkwindowssize() {
+    let height = (window.screen.availHeight)
+    let width = (window.screen.availWidth)
+
+    if (height > width) {
+        alert("Page only case access on desktop device , do you to responsiveness issue")
+        alert('Redirect to google')
+        window.location.replace('https://www.google.com')
+    }
+}
+
+
+checkwindowssize()
+
+
