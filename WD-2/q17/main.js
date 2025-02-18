c9a05708acb8e22ce4c6cbc5c4e7d1793d069e57
Its-utsav <utsavdhimmar987@gmail.com>
2025-02-18 21:39:54 +0530

    made some small changes

--- WD-2/q17/main.js
+++ WD-2/q17/main.js
@@ -1,1 +1,6 @@
-console.log("Hello to Node JS by Utsav Dhimmar")
+var http = require('http');
+http.createServer(function (req, res) {
+    res.write("<p> Hello to Node JS by Utsav Dhimmar on browser</p>");
+    console.log("Hello to Node JS by Utsav Dhimmar on node js Console");
+    res.end();
+}).listen(3000);
